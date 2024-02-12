6b765d740598ac38a16606399a364c750848e2cf
iffigueroa <isabella.figueroa@colorado.edu>
2024-02-12 15:39:49 -0800

    add initial tutorial

---
+++ README.md
@@ -0,0 +1,20 @@
+# React Tic-Tac-Toe 
+
+This project contains code from the React Tic-Tac-Toe tutorial: https://react.dev/learn/tutorial-tic-tac-toe
+
+To run tic-tac-toe clone this repo and run the following command: 
+```
+npm start
+```
+
+A localhost window will launch. 
+
+This tic-tac-toe game maintains history, so you can go back and review previous steps in the game
+
+### Improvements: 
+- Reviewing previous history is a little clunky 
+    - Example: You can pick up the game from previous steps but the history log doesn't clear until you've started playing
+    - Maybe allow user to review previous step instead? 
+    - Add undo/redo button instead 
+- Game doesn't show a draw when board is full
+- Allow users to enter names
---
+++ src/App.js
@@ -0,0 +1,121 @@
+import { useState } from 'react';
+
+function Cell({value, onCellClick}){
+  return (
+    <button className="square" onClick={onCellClick}>{value}</button>
+  );
+}
+
+function Board({xIsNext, boardValues, onPlay}) {
+  const winner = calculateWinner(boardValues)
+  let status; 
+  if(winner){
+    status = "Winner: " + winner;
+  } else {
+    status = "Next Player: "+(xIsNext? "X" : "O")
+  }
+
+  function handleClick(i){
+    console.log(boardValues)
+    if (boardValues[i] || calculateWinner(boardValues)){
+      return;
+    }
+    //Make copy of current board
+    const newBoardValues = boardValues.slice();
+    if (xIsNext){
+      newBoardValues[i] =  'X';
+    } else {
+      newBoardValues[i] =  'O';
+    }
+    onPlay(newBoardValues)
+  }
+
+  function calculateWinner(squares) {
+    console.log(squares)
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        console.log(lines[i])
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  return (
+    <>
+      <div className='status'>{status}</div>
+      <div className='board-row'>
+        <Cell value={boardValues[0]} onCellClick={() => handleClick(0)}/>
+        <Cell value={boardValues[1]} onCellClick={() => handleClick(1)}/>
+        <Cell value={boardValues[2]} onCellClick={() => handleClick(2)}/>
+      </div>
+      <div className='board-row'>
+        <Cell value={boardValues[3]} onCellClick={() => handleClick(3)}/> 
+        <Cell value={boardValues[4]} onCellClick={() => handleClick(4)}/>
+        <Cell value={boardValues[5]} onCellClick={() => handleClick(5)}/>
+      </div>
+      <div className='board-row'>
+        <Cell value={boardValues[6]} onCellClick={() => handleClick(6)}/>
+        <Cell value={boardValues[7]} onCellClick={() => handleClick(7)}/>
+        <Cell value={boardValues[8]} onCellClick={() => handleClick(8)}/>
+      </div>
+    </>
+    
+  )
+}
+
+export default function TicTacToe(){
+  // Maintain game history
+  const [currentMove, setCurrentMove] = useState(0);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentBoardValues = history[currentMove];
+  // Maintain player state (who's turn is it to move)
+  const xIsNext = currentMove % 2 == 0;
+
+  function handlePlay(updatedValues){
+    const updatedHistory = [...history.slice(0, currentMove+1), updatedValues]
+    setHistory(updatedHistory)
+    setCurrentMove(updatedHistory.length-1)
+  }
+
+  function jumpTo(nextMove){
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((boardVals, move) => {
+      let description 
+      if (move > 0){
+        description = "Go to move #" + move;
+      }else { 
+        description = 'Go to game start';
+      }
+      return (
+        <li key={move}> 
+          <button onClick={() => jumpTo(move)}>{description}</button>
+        </li>
+      )
+    }
+  )
+
+  return (
+    <div className='game'>
+      <div className='game-board'>
+        <Board xIsNext={xIsNext} boardValues={currentBoardValues} onPlay={handlePlay}/>
+      </div>
+      <div className='game-info'>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  )
+}
